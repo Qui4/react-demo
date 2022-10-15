985221f7cb39d4ae00da4d176f163e8d0a660039
Qui4 <96349796+Qui4@users.noreply.github.com>
2022-10-15 17:54:34 -0400

    made stopwatch

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,35 @@
-import logo from "./logo.svg";
+import { wait } from "@testing-library/user-event/dist/utils";
+import { React, useState, useEffect } from "react"
 import "./App.css";
 
 function App() {
-  const alert = (a) => {
-    window.alert("Hello World!"+a);
+  const [timer, setTimer] = useState(0);
+  const [timerIsActive, setTimerIsActive] = useState(false);
+
+  useEffect (() => {
+    if (timerIsActive) {
+      const interval = setInterval(() => {
+        setTimer((timer) => timer+1)
+      }, 1000)
+
+      return () => { clearInterval(interval) }
+    }
+  }, [timerIsActive, timer])
+
+  const handleStart = () => {
+    setTimerIsActive(true);
   };
+
+  const handleStop = () => {
+    setTimerIsActive(false);
+  };
+
+  const formatTime2 = (time) => {
+    const minutes = String(Math.floor(time / 60)).padStart(2, '0')
+    const seconds = String(time % 60).padStart(2, '0')
+
+    return `${minutes} : ${seconds}`
+  }
 
   return (
     <div className="App">
@@ -15,13 +40,18 @@ function App() {
       <div className="Body">
         <div className="Content">
           <div className="Timer">
-            <p className="Timer-text">00 : 00</p>
+            <p className="Timer-text">{formatTime2(timer)}</p>
           </div>
           <div className="Buttons">
-            <button className="Start Button" onClick={() => {alert("asd")}}>
+            <button
+              className="Start Button"
+              onClick={handleStart}
+            >
               <p className="Button-text">Start</p>
             </button>
-            <button className="Stop Button">
+            <button className="Stop Button"
+            onClick={handleStop}
+            >
               <p className="Button-text">Stop</p>
             </button>
           </div>
